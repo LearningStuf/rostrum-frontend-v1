ebb076f01e78af44976666841960e1e27cecf7f3
Hamza Khan <hamzakhan@Hamzas-MacBook-Pro.local>
2024-03-16 16:06:46 +0500

    added queries

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,101 +1,135 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FOOTER } from "@/quries";
 
-const Footer = () => {
+const Footer = async () => {
+  // const response = await fetch("http://localhost:1337/graphql", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     query: FOOTER,
+  //   }),
+  // });
+
+  // const {data:{footer}} = await response.json()
+  // console.log(JSON.stringify(footer) , 'SERVER DATA........')
   return (
-    <footer className='bg-primary-light'>
-      <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
-        <div className='grid grid-cols-1 gap-4 sm:gap-8 sm:grid-cols-4'>
-          <div className='mb-6 md:mb-0'>
-            <Link href='/' className='flex items-center'>
+    <footer className="bg-primary-light">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="grid grid-cols-1 gap-4 sm:gap-8 sm:grid-cols-4">
+          <div className="mb-6 md:mb-0">
+            <Link href="/" className="flex items-center">
               <Image
-                src='/assets/logo.svg'
+                src="/assets/logo.svg"
                 width={120}
                 height={120}
-                alt='Logo...'
-                className='-translate-y-16'
+                alt="Logo..."
+                className="-translate-y-16"
               />
             </Link>
-            <p className='text-[20px] text-stone -mt-6'>
+            <p className="text-[20px] text-stone -mt-6">
               Rostrum provides unparalleled services towards supporting students
               academically through the course along with assistance for
               admissions into students’ dream universities.
             </p>
           </div>
           <div>
-            <h4 className='mb-6 text-[24px] font-semibold text-stone uppercase'>
+            <h4 className="mb-6 text-[24px] font-semibold text-stone uppercase">
               Quick links
             </h4>
-            <ul className='text-gray-500 dark:text-gray-400 font-medium'>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 Boarding Schools
               </li>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 Ivy League Prep
               </li>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 Med School
               </li>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 Ox-Bridge Prep
               </li>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 SAT TUTORING
               </li>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 BUSINESS SCHOOLS
               </li>
-              <li className='mb-4 text-[20px] text-stone transition-all hover:text-secondary' role='button'>
+              <li
+                className="mb-4 text-[20px] text-stone transition-all hover:text-secondary"
+                role="button"
+              >
                 PSYCHOMETRIC TEST
               </li>
             </ul>
           </div>
           <div>
-            <h4 className='mb-6 text-[24px] font-semibold text-stone uppercase'>
+            <h4 className="mb-6 text-[24px] font-semibold text-stone uppercase">
               Address
             </h4>
-            <h4 className='mb-1 text-[24px] font-semibold text-stone'>Dubai</h4>
-            <p className='mb-6 text-[20px] text-stone'>
+            <h4 className="mb-1 text-[24px] font-semibold text-stone">Dubai</h4>
+            <p className="mb-6 text-[20px] text-stone">
               902, Citadel Tower, Business Bay, Dubai
             </p>
-            <h4 className='mb-1 text-[24px] font-semibold text-stone'>Delhi</h4>
-            <p className='text-[20px] text-stone'>
+            <h4 className="mb-1 text-[24px] font-semibold text-stone">Delhi</h4>
+            <p className="text-[20px] text-stone">
               K-317, Mehrauli - Badarpur Rd, Nai Basti, Lado Sarai, New Delhi,
               Delhi 110030
             </p>
           </div>
           <div>
-            <h4 className='mb-6 text-[24px] font-semibold text-stone uppercase'>
+            <h4 className="mb-6 text-[24px] font-semibold text-stone uppercase">
               Contact us
             </h4>
-            <h4 className='mb-1 text-[24px] font-semibold text-stone'>Phone</h4>
-            <ul className='text-gray-500 dark:text-gray-400 font-medium'>
-              <li className='mb-1 text-[20px] text-stone' role='button'>
+            <h4 className="mb-1 text-[24px] font-semibold text-stone">Phone</h4>
+            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+              <li className="mb-1 text-[20px] text-stone" role="button">
                 UAE : +971 50 939 5354
               </li>
-              <li className='mb-1 text-[20px] text-stone ' role='button'>
+              <li className="mb-1 text-[20px] text-stone " role="button">
                 IND : +91 93100 79937
               </li>
-              <li className='mb-1 text-[20px] text-stone ' role='button'>
+              <li className="mb-1 text-[20px] text-stone " role="button">
                 IND : +91 124 426 2020
               </li>
-              <li className='mb-1 text-[20px] text-stone ' role='button'>
+              <li className="mb-1 text-[20px] text-stone " role="button">
                 IND : +91 11 4019 9312
               </li>
             </ul>
-            <h4 className='mb-1 mt-6 text-[24px] font-semibold text-stone'>
+            <h4 className="mb-1 mt-6 text-[24px] font-semibold text-stone">
               Email address
             </h4>
-            <p className='mb-6 text-[20px] text-stone'>info@rostrumedu.com</p>
+            <p className="mb-6 text-[20px] text-stone">info@rostrumedu.com</p>
           </div>
         </div>
       </div>
-      <hr className='mx-auto w-full max-w-screen-xl my-4 border-primary-dark sm:mx-auto lg:mt-4' />
-      <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:pb-5'>
-        <span className='text-[20px] text-stone sm:text-center'>
-          © 2024{' '}
-          <Link href='/' className='hover:underline'>
+      <hr className="mx-auto w-full max-w-screen-xl my-4 border-primary-dark sm:mx-auto lg:mt-4" />
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:pb-5">
+        <span className="text-[20px] text-stone sm:text-center">
+          © 2024{" "}
+          <Link href="/" className="hover:underline">
             Rostum
           </Link>
           . All Rights Reserved.
---
+++ src/quries/index.js
@@ -0,0 +1,363 @@
+export const HOME_PAGE = `
+query HomePage {
+    homePage {
+      data {
+        id
+        attributes {
+          headerSection {
+            id
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+            text
+          }
+          serviceSection {
+            description
+            id
+            title
+          }
+          statsSection {
+            id
+            number1
+            number2
+            number3
+            number4
+            text1
+            text3
+            text2
+            text4
+          }
+          callToAction {
+            headline
+            id
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const HOME_SERVICES = `
+query ServicesHomes {
+    servicesHomes {
+      data {
+        id
+        attributes {
+          title
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const HOME_VIDEO_TESTIMONIALS = `
+query VideoTestimonials {
+    videoTestimonials {
+      data {
+        attributes {
+          name
+          thumbnail {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+          university
+          video {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const HOME_TESTIMONIALS = `
+query Testimonials {
+    testimonials {
+      data {
+        attributes {
+          description
+          name
+          university
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const HOME_FEATURED = `
+query Featureds {
+    featureds {
+      data {
+        attributes {
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const HOME_UNIVERSITIES = `
+query UniversitiesHomes {
+    universitiesHomes {
+      data {
+        attributes {
+          name
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const COMPARISON_TABLE = `
+query ComparisonTables {
+    comparisonTables {
+      data {
+        attributes {
+          generalAdmissionRate
+          rostumAdmissionRate
+          university
+        }
+      }
+    }
+  }
+`;
+
+export const FOOTER = `
+query Footer {
+    footer {
+      data {
+        attributes {
+          addressDelhi
+          addressDubai
+          description
+          email
+          phone1
+          phone2
+          phone3
+          phone4
+          website_routes {
+            data {
+              attributes {
+                name
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const ABOUT_PAGE = `
+query About {
+    about {
+      data {
+        attributes {
+          weWorkText
+          missionSection {
+            description_1
+            description_2
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+          headerSection {
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+          callToAction {
+            headline
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+            title
+          }
+          weWorkSection {
+            description
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const TEAM_PAGE = `
+query TeamPage {
+    teamPage {
+      data {
+        attributes {
+          headerSection {
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+            title
+          }
+          teams {
+            description
+            designation
+            name
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const REVIEW_PAGE = `
+query ReviewsPage {
+    reviewsPage {
+      data {
+        attributes {
+          headerSection {
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+          reviews {
+            description
+            designation
+            name
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const BLOG_PAGE = `
+query BlogPage {
+    blogPage {
+      data {
+        attributes {
+          headerSection {
+            title
+            image {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const ALL_BLOGS = `
+query Blogs {
+    blogs {
+      data {
+        attributes {
+          createdAt
+          description
+          slug
+          title
+          content
+          image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`;
